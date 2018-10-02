3d2775f534c90c38ddf47f504f658885f4e2d938
Ardianpurnama <31305690+Ardianpurnama@users.noreply.github.com>
2018-10-02 09:24:39 +0700

    Add files via upload

---
+++ studen.py
@@ -0,0 +1,27 @@
+# -*- : root@Ardian_Purnama94:~# python anounce.py
+import wikipedia
+help(wikipedia)
+print("RFU SEKAWAN")
+
+#contoh penjumlahan, dan cetak hasilnya
+jumlah = 10 + 10
+print (jumlah)
+
+eggs = 10 ** 2
+print (eggs)
+ 
+#contoh lain
+pejumlahan = 72 + 23
+pengurangan = 108 - 204
+perkalian = 108 * 0.5
+pembagian = 108 / 9
+
+""" Ini adalah contoh
+Komentar multi-baris
+pada Python """
+my_var = 123
+
+def spam():
+    eggs = 12
+    return eggs
+
